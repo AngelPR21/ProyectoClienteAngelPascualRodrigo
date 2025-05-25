8ac349ae6eebf30d0eaddf9f59f70c06d65447d8
angel <angpasrod2@alu.edu.gva.es>
2025-05-25 14:55:18 +0200

    V0

---
+++ app/src/main/java/es/ieslavereda/baseoficios/base/Parameters.java
@@ -0,0 +1,8 @@
+package es.ieslavereda.baseoficios.base;
+
+public class Parameters {
+
+    public static final String URL_API_BASE = "http://192.168.1.12:8080"; // SUSTITUIR IP
+    public static final String URL_IMAGE_BASE = "https://my-web.joaalsai.com/images/";
+
+}
