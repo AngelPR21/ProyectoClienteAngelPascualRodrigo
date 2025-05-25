d1229bf655034d3e036d3fa94c069a672af44519
angel <angpasrod2@alu.edu.gva.es>
2025-05-25 14:53:39 +0200

    V0

---
+++ app/src/main/java/es/ieslavereda/baseoficios/activities/model/Usuario.java
@@ -0,0 +1,55 @@
+package es.ieslavereda.baseoficios.activities.model;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private int idUsuario;
+    private String nombre;
+    private String apellidos;
+    private int idOficio;
+
+    public Usuario(String nombre, String apellidos, int idOficio) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.idOficio = idOficio;
+    }
+
+    public Usuario(int idUsuario, String nombre, String apellidos, int idOficio) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.idOficio = idOficio;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getOficioIdOficio() {
+        return idOficio;
+    }
+
+    public void setIdOficio(int Oficio_idOficio) {
+        Oficio_idOficio = Oficio_idOficio;
+    }
+}
